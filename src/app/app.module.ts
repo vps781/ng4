7a8a5caa44de202f4a7b233135ac70cc2d24765e
Vijay kumar yadav <vijayakumar@gmcads.com>
2017-10-08 01:04:27 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { SampleComponent } from 'vijay';
 
 import { AppComponent } from './app.component';
+import { HeroDetailsComponent } from './hero-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [SampleComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
